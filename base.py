8cea6f37d0be5d1217537026a51f3a099a66b3d4
None <None>
2025-02-26 10:59:50 -0800

    txts added

--- base.py
+++ base.py
@@ -43,9 +43,9 @@ class CustomTopo(Topo):
         self.addLink(h7, s4)
 
         # Connect switches with given bandwidth and loss settings
-        self.addLink(s1, s2)
-        self.addLink(s2, s3)
-        self.addLink(s3, s4)
+        self.addLink(s1, s2, bw=50)
+        self.addLink(s2, s3, bw=50)
+        self.addLink(s3, s4, bw=50)
 
 
 topos = { 'mytopo': ( lambda: CustomTopo() ) }
